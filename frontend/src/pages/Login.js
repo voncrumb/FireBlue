a7be217bf6f0b90d0a0b058a9e49e10f43b50929
voncrumb <sackboy129@gmail.com>
2020-10-17 14:31:02 +1000

    Finished Login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,16 +3,18 @@ import './Login.css';
 
 import api from '../services/api';
 
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 
 export default function Login({ history }) {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     const response = await api.post('/devs', {
       username,
+      password
     });
 
     const { _id } = response.data;
@@ -23,13 +25,18 @@ export default function Login({ history }) {
   return (
     <div className="login-container">
       <form onSubmit={handleSubmit}>
-        <img src={logo} alt="Tindev"/>
+        <img className="logo" src={logo} alt="Tindev" />
         <input 
-          placeholder="Digite seu usuário no Github"
+          placeholder="LinkedIn Username / Email"
           value={username}
           onChange={e => setUsername(e.target.value)}
         />
-        <button type="submit">Enviar</button>
+        <input 
+          placeholder="LinkedIn Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
